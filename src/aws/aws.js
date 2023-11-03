33fa15d4826fdc9c74363a9b0dac146fc20aaab5
Josue Marquez <owaruuu@gmail.com>
2023-11-02 22:21:36 -0300

    Update aws.js
    -move authenticateUser function to aws.js

--- src/aws/aws.js
+++ src/aws/aws.js
@@ -11,7 +11,29 @@ export const connectCognito = async () => {
 
         return response.data;
     } catch (error) {
-        console.log("error with app server: ", error);
+        // console.log("error with app server: ", error);
         return { msg: "error with app server", value: -1 };
+    }
+};
+
+//revisa si el usuario existe en el userpool de cognito
+export const aunthenticateUser = async (email, password) => {
+    try {
+        const response = await api.post("http://localhost:3003/login", {
+            email,
+            password,
+        });
+        console.log(
+            "🚀 ~ file: aws.js:26 ~ aunthenticateUser ~ response:",
+            response
+        );
+
+        return response;
+    } catch (error) {
+        console.log("🚀 ~ file: aws.js:29 ~ aunthenticateUser ~ error:", error);
+
+        //errores pueden incluir que el server este caido o algun error en las credenciales
+
+        throw error;
     }
 };
