eac381cbae74cb0d15b289d3bbb81ba81a7bd2cc
Josue Marquez <owaruuu@gmail.com>
2023-11-06 13:02:35 -0300

    Update Header.js
    -add return to main screen function to header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import LoginHeader from "./LoginHeader";
 import InfoHeader from "./Header/InfoHeader";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Header = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleClick = () => {
+        dispatch({ type: "CHANGE_SCREEN", payload: { currentScreen: "main" } });
+    };
+
     return (
         <header className="header">
-            <h1>React Flashcards</h1>
+            <h1 className="pageTitle" onClick={handleClick}>
+                React Flashcards
+            </h1>
             <InfoHeader />
             <LoginHeader />
         </header>
