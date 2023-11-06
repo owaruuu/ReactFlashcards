7b8b74a0b8ef2677b84ad44039081edd5b0bbdc6
Josue Marquez <owaruuu@gmail.com>
2023-11-06 13:11:50 -0300

    Update LearnScreen.js
    -add back button
    -add disappearing cards
    -add blocked state after user clicks learn buttons
    -both learn buttons now use the same function
    -add effect to block state to reset it after 1 second
    -add logic to only show progress bar and learn buttons when theres user progress

--- src/components/LearnScreen/LearnScreen.js
+++ src/components/LearnScreen/LearnScreen.js
@@ -1,6 +1,8 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
 import TermCard from "./TermCard";
+import DisappearingCard from "./DisappearingCard";
+import BackButton from "../BackButton";
 import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 import { lectures } from "../../data/lectures";
 
@@ -13,24 +15,35 @@ const LearnScreen = () => {
 
     const [lecture, setLecture] = useState(currentLecture);
     const [terms, setTerms] = useState(currentLecture.termList);
-    console.log("🚀 ~ file: LearnScreen.js:16 ~ LearnScreen ~ terms:", terms);
     const [index, setIndex] = useState(0);
     const [showAnswer, setShowAnswer] = useState(false);
+    const [disappearingCards, setDisappearingCards] = useState([]);
+    const [blocked, setBlocked] = useState(false);
 
-    console.log(
-        "🚀 ~ file: LearnScreen.js:9 ~ LearnScreen ~ user:",
-        user.currentProgress
-    );
-
-    {
-        //obtener id del current term
-        //dependiendo del boton, agregar o modificar el user.currentProgress
-        //con un valor de "learned" o 'learning'
-    }
+    const removeDisappearingCard = (id) => {
+        setDisappearingCards((prevCards) =>
+            prevCards.filter((card) => {
+                console.log(card.key, id);
+                return Number(card.key) !== id;
+            })
+        );
+    };
 
     const goBack = () => {
         console.log("click back");
         setShowAnswer(false);
+
+        setDisappearingCards([
+            <DisappearingCard
+                key={index}
+                terms={lecture.termList}
+                index={index}
+                showAnswer={showAnswer}
+                killFunc={() => removeDisappearingCard(index)}
+                direction={" disappear-right"}
+            />,
+            ...disappearingCards,
+        ]);
 
         if (index - 1 < 0) {
             setIndex(lecture.termList.length - 1);
@@ -40,8 +53,19 @@ const LearnScreen = () => {
     };
 
     const goForward = () => {
-        console.log("click forward");
         setShowAnswer(false);
+
+        setDisappearingCards([
+            <DisappearingCard
+                key={index}
+                terms={lecture.termList}
+                index={index}
+                showAnswer={showAnswer}
+                killFunc={() => removeDisappearingCard(index)}
+                direction={" disappear-left"}
+            />,
+            ...disappearingCards,
+        ]);
 
         if (index + 1 > lecture.termList.length - 1) {
             setIndex(0);
@@ -54,7 +78,8 @@ const LearnScreen = () => {
         setShowAnswer((prevState) => !prevState);
     };
 
-    const handleLearning = () => {
+    const handleLearnButton = (value) => {
+        setBlocked(true);
         //obtener id del current term
         const currentTermId = terms[index].id;
 
@@ -67,99 +92,98 @@ const LearnScreen = () => {
                     ...user.currentProgress,
                     [lecture.lectureId]: {
                         ...user.currentProgress[lecture.lectureId],
-                        [currentTermId]: "learning",
+                        [currentTermId]: value,
                     },
                 },
             },
         });
     };
 
-    const handleLearned = () => {
-        //obtener id del current term
-        const currentTermId = terms[index].id;
-
-        //dependiendo del boton, agregar o modificar el user.currentProgress
-        //con un valor de "learned" o 'learning'
-        dispatch({
-            type: "UPDATE_PROGRESS",
-            payload: {
-                currentProgress: {
-                    ...user.currentProgress,
-                    [lecture.lectureId]: {
-                        ...user.currentProgress[lecture.lectureId],
-                        [currentTermId]: "learned",
-                    },
-                },
-            },
-        });
-    };
+    useEffect(() => {
+        if (blocked) {
+            const timer = setTimeout(() => {
+                goForward();
+                setBlocked(false);
+            }, 1000);
+            return () => clearTimeout(timer);
+        }
+    }, [blocked]);
 
     const progressCells = terms.map((term, termIndex) => {
         // console.log(term.id);
 
         let classNames = "progressBarItem";
 
-        const termState = user.currentProgress[lecture.lectureId]?.[term.id];
-        console.log(
-            "🚀 ~ file: LearnScreen.js:101 ~ progressCells ~ termState:",
-            termState
-        );
+        const termState = user.currentProgress?.[lecture.lectureId]?.[term.id];
 
         if (termState) {
             classNames += ` ${termState}`;
+        } else {
+            return;
         }
 
         if (termIndex == index) {
-            console.log("entre aqui");
+            // console.log("entre aqui");
             classNames += " activeItem";
         }
         return <div key={term.id} className={classNames}></div>;
     });
 
+    const learnButtons = [
+        {
+            text: "Learning",
+            id: "learning",
+            classes: ["learningButton", "learningButton learning"],
+        },
+        {
+            text: "Learned",
+            id: "learned",
+            classes: ["learnedButton", "learnedButton learned"],
+        },
+    ];
+
     return (
         <div className="learnScreen">
             <h2>{lecture.name}</h2>
-            <div className="progressBar">{progressCells}</div>
+            <BackButton options={{ currentScreen: "lecture" }} />
+            {user.currentProgress && (
+                <div className="progressBar">{progressCells}</div>
+            )}
             <div className="termCardSection">
                 <button onClick={goBack}>
                     <BiLeftArrow></BiLeftArrow>
                 </button>
-                <TermCard
-                    terms={lecture.termList}
-                    index={index}
-                    showAnswer={showAnswer}
-                    answerFunction={handleClick}
-                />
+                <div className="termCardDiv">
+                    <TermCard
+                        terms={lecture.termList}
+                        index={index}
+                        showAnswer={showAnswer}
+                        answerFunction={handleClick}
+                    />
+                    {disappearingCards}
+                </div>
+
                 <button onClick={goForward}>
                     <BiRightArrow></BiRightArrow>
                 </button>
             </div>
             <div className="learnButtons">
-                <button
-                    // className="learningButton learning"
-                    className={
-                        user.currentProgress[lecture.lectureId]?.[
-                            terms[index].id
-                        ] === "learning"
-                            ? "learningButton learning"
-                            : "learningButton"
-                    }
-                    onClick={handleLearning}
-                >
-                    Learning
-                </button>
-                <button
-                    className={
-                        user.currentProgress[lecture.lectureId]?.[
-                            terms[index].id
-                        ] === "learned"
-                            ? "learnedButton learned"
-                            : "learnedButton"
-                    }
-                    onClick={handleLearned}
-                >
-                    Learned
-                </button>
+                {user.currentProgress &&
+                    learnButtons.map((button) => (
+                        <button
+                            className={
+                                user.currentProgress[lecture.lectureId]?.[
+                                    terms[index].id
+                                ] === button.id
+                                    ? button.classes[1]
+                                    : button.classes[0]
+                            }
+                            onClick={() => handleLearnButton(button.id)}
+                            disabled={blocked}
+                        >
+                            {button.text}
+                        </button>
+                    ))}
             </div>
         </div>
     );
