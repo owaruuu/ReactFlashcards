22be5e2aa0d5cf7eb2911e6bd01b4f4e85fe43d1
Josue Marquez <owaruuu@gmail.com>
2023-11-06 13:14:38 -0300

    Update LoginControls.js
    -add new error checking for cognito error and login server error and db error
    -move conditional logic to component

--- src/components/LoginControls.js
+++ src/components/LoginControls.js
@@ -4,11 +4,20 @@ import { connectCognito, getUserProgress, quickScan } from "../aws/aws";
 import Spinner from "react-bootstrap/Spinner";
 
 const LoginControls = () => {
-    const { dispatch, cognito, cognitoError, init, loggedIn, user } =
-        useContext(AppContext);
+    const {
+        dispatch,
+        cognito,
+        cognitoError,
+        dbError,
+        init,
+        loggedIn,
+        user,
+        serverError,
+    } = useContext(AppContext);
 
     const [loggedin, setLoggedin] = useState(false);
     const [userName, setUserNAme] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     // const [loaded, setUserNAme] = useState(false);
 
     //intento hacer login
@@ -25,7 +34,15 @@ const LoginControls = () => {
 
             //si la respuesta es -1 significa que hubo un problema con el server
             if (response.value === -1) {
+                // dispatch({ type: "SET_COGNITO_ERROR", payload: true });
+                dispatch({ type: "SET_SERVER_ERROR", payload: true });
+                setErrorMessage("Server error, try refreshing the page.");
+                return;
+            }
+
+            if (response.value === -2) {
                 dispatch({ type: "SET_COGNITO_ERROR", payload: true });
+                setErrorMessage("Cognito server error, try again later.");
                 return;
             }
 
@@ -56,13 +73,19 @@ const LoginControls = () => {
                         currentProgress: JSON.parse(progress),
                     },
                 });
+            } else {
+                console.log("no hay db y deberia desactivar cosas");
+                dispatch({ type: "SET_DB_ERROR", payload: true });
+                setErrorMessage(
+                    "Database server error try refreshing the page."
+                );
             }
         };
 
         loginStatus();
     }, []);
 
-    const errorMsg = <p>Server down. Trying again later.</p>;
+    const errorMsg = <p>{errorMessage}</p>;
 
     const loggedInControls = <button>{user.userName}</button>;
 
@@ -97,19 +120,38 @@ const LoginControls = () => {
         </>
     );
 
-    return (
-        <>
-            {!init ? (
+    const controls = () => {
+        if (!init) {
+            return (
                 <Spinner animation="border" role="status">
                     <span className="visually-hidden">Loading...</span>
                 </Spinner>
-            ) : cognitoError ? (
-                errorMsg
+            );
+        }
+
+        if (serverError || dbError) {
+            return errorMsg;
+        }
+
+        if (loggedIn) {
+            return loggedInControls;
+        } else {
+            return loggedOutControls;
+        }
+    };
+
+    return (
+        <>
+            {controls()}
+            {/* {!init ? (
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
             ) : loggedIn ? (
                 loggedInControls
             ) : (
                 loggedOutControls
-            )}
+            )} */}
         </>
     );
 };
