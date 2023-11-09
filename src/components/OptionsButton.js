98c8bf76e66d023d3dfd26a304932cbaf1326b15
Josue Marquez <owaruuu@gmail.com>
2023-11-09 01:49:54 -0300

    Updates

--- src/components/OptionsButton.js
+++
@@ -1,9 +0,0 @@
-const OptionsButton = (props) => {
-    return (
-        <button className="optionsButton" onClick={props.showFunc}>
-            Options
-        </button>
-    );
-};
-
-export default OptionsButton;
