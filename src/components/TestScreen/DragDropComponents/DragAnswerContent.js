48eceeee76b693e311bc26f1039f6b1d0a7d344f
Josue Marquez <owaruuu@gmail.com>
2023-11-29 19:27:18 -0300

    dragAnswer update
    -add module style to control animation

--- src/components/TestScreen/DragDropComponents/DragAnswerContent.js
+++ src/components/TestScreen/DragDropComponents/DragAnswerContent.js
@@ -1,5 +1,17 @@
+import classNames from "classnames";
+import styles from "./styles/DragAnswer.module.css";
+
 const DragAnswerContent = (props) => {
-    const content = <span>{props.phrase}</span>;
+    const content = (
+        <span
+            className={classNames(
+                styles.dragAnswer,
+                props.changed && styles.blinkAnimation
+            )}
+        >
+            {props.phrase}
+        </span>
+    );
     return <>{props.phrase !== "" ? content : "..."}</>;
 };
 
