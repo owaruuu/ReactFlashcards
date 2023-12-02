384805d929dedec99c890c7e51f59081f0e2c2d3
Josue Marquez <owaruuu@gmail.com>
2023-12-02 10:36:00 -0300

    Update Header.js
    -modify header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
     return (
         <header className="header">
             <h1 className="pageTitle" onClick={handleClick}>
-                React Flashcards
+                -React Flashcards-
             </h1>
             <LoginHeader />
         </header>
