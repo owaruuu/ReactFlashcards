41ee115c62905938efb9b7fe6c1c9d9fdd951f35
Josue Marquez <owaruuu@gmail.com>
2024-04-04 19:43:49 -0300

    Create fake-db.js

---
+++ src/data/fake-db.js
@@ -0,0 +1,287 @@
+export const userData = {
+    //user id
+    123: {
+        //lecture id
+        1: {
+            //terms options
+            terms: {
+                1: {
+                    modifier: "muted",
+                },
+                2: {
+                    modifier: "highlighted",
+                },
+            },
+            //Informacion sobre el review 2.0
+            currentSession: {
+                currentIndex: 1,
+                terms: ["1", "2", "3", "4"],
+            },
+        },
+    },
+};
+
+export const userQuizProgress = {
+    //id user
+    123: {
+        //id lecture
+        1: {
+            //term info
+            japaneseTerms: {
+                1: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                2: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                3: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                4: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                5: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                6: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                7: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                8: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                9: {
+                    level: "master",
+                    nextQuiz: new Date(),
+                },
+                10: {
+                    level: "master",
+                    nextQuiz: new Date(),
+                },
+                11: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                12: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                13: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                14: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                15: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                16: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                17: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                18: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                19: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                20: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                21: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                22: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                23: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                24: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                25: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+            },
+            spanishTerms: {
+                1: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                2: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                3: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                4: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                5: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+            },
+            japaneseQuizQueue: ["6", "7", "12"],
+            spanishQuizQueue: ["6", "7", "12"],
+        },
+        2: {
+            //term info
+            japaneseTerms: {
+                1: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                2: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                3: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                4: {
+                    level: "gold",
+                    nextQuiz: new Date(),
+                },
+                5: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                6: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                7: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                8: {
+                    level: "diamond",
+                    nextQuiz: new Date(),
+                },
+                9: {
+                    level: "master",
+                    nextQuiz: new Date(),
+                },
+                10: {
+                    level: "master",
+                    nextQuiz: new Date(),
+                },
+                11: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                12: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                13: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                14: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                15: {
+                    level: "bronze",
+                    nextQuiz: new Date(),
+                },
+                16: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                17: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                18: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                19: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                20: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                21: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                22: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                23: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                24: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+                25: {
+                    level: "platinum",
+                    nextQuiz: new Date(),
+                },
+            },
+            spanishTerms: {
+                1: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                2: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                3: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                4: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+                5: {
+                    level: "silver",
+                    nextQuiz: new Date(),
+                },
+            },
+            japaneseQuizQueue: ["6", "7", "12"],
+            spanishQuizQueue: ["6", "7", "12"],
+        },
+    },
+};
