97ef30350940dee4c4c62b293e2dc4a170a8c0fa
Josue Marquez <owaruuu@gmail.com>
2024-05-03 18:45:44 -0400

    test new server
    -fix theme error

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export let URL = "https://service.owaruuu.xyz";
-// URL = "http://localhost:3003";
+export let URL = "https://api.owaruuu.xyz";
+// URL = "https://service.owaruuu.xyz";
+// URL = "https://localhost:443";
 
 export const api = axios.create({
     withCredentials: true, // Include credentials (cookies) in the request
