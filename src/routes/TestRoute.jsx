3edc728ffa632a4b6245bd14ae54c4f741801686
Josue Marquez <owaruuu@gmail.com>
2024-07-01 13:05:48 -0400

    Update TestRoute.jsx
    -clean TestRoute now that test components and logic has its own file and route

--- src/routes/TestRoute.jsx
+++ src/routes/TestRoute.jsx
@@ -1,54 +1,19 @@
 import React, { useContext, useState } from "react";
 import { Link, Outlet, useOutletContext } from "react-router-dom";
-import ProblemCounter from "../components/TestScreen/ProblemCounter";
-import { tests } from "../data/tests";
-import {
-    getRandomMondai,
-    chooseThreeDrag,
-    getRandomNumbersSimple,
-} from "../utils/utils";
 import { AppContext } from "../context/AppContext";
-import TryTestView from "./views/TryTestView";
 
 const TestRoute = () => {
     const { user } = useContext(AppContext);
     const { test, hasTest, lecture } = useOutletContext();
-    // console.log("🚀 ~ TestRoute ~ test:", test);
 
     //State
     const [isTakingTest, setIsTakingTest] = useState(false);
     console.log("🚀 ~ TestRoute ~ isTakingTest:", isTakingTest);
-    const [stage, setStage] = useState("begin");
-    const [score, setScore] = useState(0);
-    const [stopTimer, setStopTimer] = useState(false);
-    const [timerInfo, setTimerInfo] = useState({
-        totalSeconds: 0,
-        seconds: 0,
-        minutes: 0,
-        hours: 0,
-    });
-    const [fiveMondai] = useState(() => getRandomMondai(test));
-    const [threeDrag] = useState(() =>
-        chooseThreeDrag(
-            test,
-            getRandomNumbersSimple(
-                test.dragOptions.quantity,
-                test.dragDrop.length
-            )
-        )
-    );
-    const [currentTest] = useState({
-        version: test.version,
-        mondai: fiveMondai,
-        dragDrop: threeDrag,
-        manga: [],
-    });
 
     //Vars
     const lastTestResults =
         user.currentProgress[lecture.lectureId]?.["lastTest"];
     const highScore = user.currentProgress[lecture.lectureId]?.["highScore"];
-    const goldAccent = <span className="goldAccent">:</span>;
     const [hasWonMedal] = useState(() => {
         const hasMedal = user.currentProgress.stickers?.[lecture.lectureId];
 
@@ -59,70 +24,7 @@ const TestRoute = () => {
         }
     });
 
-    const showTimer =
-        stage === "mondai" ||
-        stage === "dragDrop" ||
-        stage === "manga" ||
-        stage === "results"
-            ? true
-            : false;
-
-    // const title =
-    //     stage === "begin" ? (
-    //         <h3>Prueba Corta</h3>
-    //     ) : stage === "last" ? (
-    //         <h3>Ultimo intento</h3>
-    //     ) : stage === "high" ? (
-    //         <h3>Tu record{goldAccent}</h3>
-    //     ) : stage === "mondai" ? (
-    //         getInstruction("mondai")
-    //     ) : stage === "dragDrop" ? (
-    //         getInstruction("drag")
-    //     ) : stage === "manga" ? (
-    //         <h3>Sigue la conversacion</h3>
-    //     ) : (
-    //         <h3>
-    //             Tus resultados
-    //             {goldAccent}
-    //         </h3>
-    //     );
-
-    // const showPoints =
-    //     stage === "mondai" || stage === "dragDrop" || stage === "manga"
-    //         ? true
-    //         : false;
-
-    const currentMax =
-        stage === "mondai"
-            ? currentTest.mondai.length
-            : stage === "dragDrop"
-            ? currentTest.dragDrop.length
-            : currentTest.manga.length;
-
-    //Functions
-    const updateTestTime = (info) => {
-        setTimerInfo(info);
-    };
-
-    const getInstruction = (type) => {
-        switch (type) {
-            case "mondai":
-                return test.mondaiTitle;
-            case "drag":
-                return test.dragTitle;
-            default:
-                console.error("unknown type " + type);
-                break;
-        }
-    };
-
     //elements
-    const pointsCounter = (
-        <div className="pointsCounter">
-            <p>{score}pts.</p>
-        </div>
-    );
-
     if (!hasTest) {
         return (
             <div style={{ color: "white" }}>
@@ -137,34 +39,9 @@ const TestRoute = () => {
     return (
         <div className="testScreen">
             <h2 className="testTitle">
-                <p>
-                    Prueba - {lecture.name}{" "}
-                    {/* {showTimer && (
-                        <TestTimer
-                            stopTimer={stopTimer}
-                            updateTime={updateTestTime}
-                        ></TestTimer>
-                    )} */}
-                </p>
+                <p>Prueba - {lecture.name}</p>
             </h2>
-
-            {/* {isTakingTest ? (
-                <TryTestView test={test} lecture={lecture} />
-            ) : (
-                <Outlet
-                    context={{
-                        test,
-                        lastTestResults,
-                        highScore,
-                        hasWonMedal,
-                        lecture,
-                        lectureId: lecture.lectureId,
-                        isTakingTest,
-                        setIsTakingTest,
-                    }}
-                />
-            )} */}
-
+            {isTakingTest && <p>is taking test</p>}
             <Outlet
                 context={{
                     test,
