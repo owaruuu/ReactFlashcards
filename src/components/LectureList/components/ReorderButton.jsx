31c6f1bc870f4f077f039bae88ee73405c982308
Josue Marquez <owaruuu@gmail.com>
2024-07-05 19:35:46 -0400

    reordering arrow update

--- src/components/LectureList/components/ReorderButton.jsx
+++ src/components/LectureList/components/ReorderButton.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { MdArrowDropDown } from "react-icons/md";
 import { MdArrowDropUp } from "react-icons/md";
+import { TiArrowSortedUp } from "react-icons/ti";
+import { TiArrowSortedDown } from "react-icons/ti";
 
 const ReorderButton = ({ name, text, state, onClick, callback }) => {
     const arrow =
         state === null ? (
             <div className="emptyDiv"></div>
         ) : state === "ASC" ? (
-            <MdArrowDropUp />
+            <TiArrowSortedUp />
         ) : (
-            <MdArrowDropDown />
+            <TiArrowSortedDown />
         );
 
     return (
