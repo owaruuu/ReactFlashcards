7f14989d927719ce6fffb250719486a199c4ac11
Josue Marquez <owaruuu@gmail.com>
2026-04-06 17:13:33 -0400

    Update version.js

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,3 +1,4 @@
 export const version =
-    "12/7/2025 - add proper support to read esp and jap example sentences from database";
+    "6/4/2026 - move all user-data routes to api/v2 to work with path cookies";
+// "12/7/2025 - add proper support to read esp and jap example sentences from database";
 // "fix bug with studied amount, fix bug with session creation, add points to kanji cards - 2/06/2025"
