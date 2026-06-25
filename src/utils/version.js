38603c782fd47b8ab6c58f68ab9bea48c4219999
Josue Marquez <owaruuu@gmail.com>
2026-06-25 15:57:45 -0400

    Update version.js

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,4 +1,5 @@
-export const version = "24/6/2026 - update vite to 8.1.0";
+export const version = "25/6/2026 - move amount can learn to lecture route";
+// "24/6/2026 - update vite to 8.1.0";
 //  "23/6/2026 - finish adding progress bar to lecture buttons, fix bug with logged out user";
 // "15/6/2026 - add back kanji lectures";
 // "12/6/2026 - add favorite lectures filter";
