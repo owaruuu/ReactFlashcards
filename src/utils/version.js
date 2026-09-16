17ee583efc840e7e28fec3f6114614310ff98489
Josue Marquez <owaruuu@gmail.com>
2026-09-15 22:19:18 -0300

    update some dependencies
    move types to current react version
    update some simple dependencies

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,5 +1,5 @@
 export const version = 
-"15/9/2026 - update react to 19.2, update to react query 5, with @tanstack/react-query";
+"15/9/2026 - update react to 19.2, update to react query 5, with @tanstack/react-query, update some dependencies";
 // "09/9/2026 - update react to 19.2";
 // "08/9/2026 - update levels code, bumped max level to 10";
 // "03/9/2026 - update react to 18.3";
