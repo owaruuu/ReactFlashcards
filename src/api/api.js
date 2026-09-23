44ef8b6821966e88b8ae6be362f500be9d001ef2
Josue Marquez <owaruuu@gmail.com>
2026-09-22 22:33:04 -0300

    update version
    remove unnecessary log
    source control prettierignore file

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export let URL = import.meta.env.VITE_API_URL;
-console.log("🚀 ~ URL:", URL);
 
 export const cloudFrontURL = "https://d2x9kal0r3kbk6.cloudfront.net";
 
--- src/utils/version.js
+++ src/utils/version.js
@@ -1,5 +1,6 @@
 export const version = 
-"19/9/2026 - move date utils to separate file";
+"22/9/2026 - refactor filtering code";
+// "19/9/2026 - move date utils to separate file";
 // "16/9/2026 - fix session onSuccess bug with new array for mutation key";
 // "15/9/2026 - update react to 19.2, update to react query 5, with @tanstack/react-query, update some dependencies";
 // "09/9/2026 - update react to 19.2";
